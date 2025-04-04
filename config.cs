372d86c4add3d8fc08e52c1238adfe0a2707c247
Tymek00 <tyja3017@gmail.com>
2025-04-04 21:11:27 +0200

    Add files via upload

---
+++ config.cs
@@ -0,0 +1,24 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+using System.Collections.Generic;
+using PlayerRoles;
+
+namespace SelectiveTeslas
+{
+    public class Config : IConfig
+    {
+        [Description("Włącza lub wyłącza plugin.")]
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("Debug Mode.")]
+        public bool Debug { get; set; } = false;
+
+        [Description("Lista teamow, które POWINNY aktywować bramki Tesli.")]
+        public HashSet<Team> ActivatingTeams { get; set; } = new HashSet<Team>
+        {
+            Team.ChaosInsurgency,
+            Team.ClassD,
+            Team.SCPs
+        };
+    }
+}
---
+++ plugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+
+using SelectiveTeslas.Handlers;
+using PlayerEvents = Exiled.Events.Handlers.Player;
+
+namespace SelectiveTeslas
+{
+    public class SelectiveTeslasPlugin : Plugin<Config>
+    {
+        public override string Name => "TeslaAdvanced";
+        public override string Author => "Tymek";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 5, 1); 
+
+        public static SelectiveTeslasPlugin Instance { get; private set; }
+
+        private PlayerHandlers _playerHandlers;
+
+        public override PluginPriority Priority => PluginPriority.Low;
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            _playerHandlers = new PlayerHandlers(Config);
+
+            RegisterEvents();
+
+            Log.Info($"{Name} v{Version} by {Author} został włączony. Tesle aktywują się tylko dla: {string.Join(", ", Config.ActivatingTeams)}.");
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+            _playerHandlers = null;
+            Instance = null;
+
+            Log.Info($"{Name} został wyłączony. Tesle działają normalnie.");
+            base.OnDisabled();
+        }
+
+        private void RegisterEvents()
+        {
+            PlayerEvents.TriggeringTesla += _playerHandlers.OnTriggeringTesla;
+            if (Config.Debug) Log.Debug($"{Name}: Zarejestrowano event TriggeringTesla.");
+        }
+
+        private void UnregisterEvents()
+        {
+            PlayerEvents.TriggeringTesla -= _playerHandlers.OnTriggeringTesla;
+            if (Config.Debug) Log.Debug($"{Name}: Wyrejestrowano event TriggeringTesla.");
+        }
+    }
+}
